9db56091be495cc9f6d6864cbb3fcb0dd8d3590d
Jose <joseamelendez1301@gmail.com>
2023-09-22 10:16:47 -0400

    completed array lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+function appendCat(name){
+    let newCats = [...cats,name]; 
+    return newCats
+}   
+function prependCat(name){
+    let newnewCats = [name,...cats];
+    return newnewCats
+}
+function removeLastCat(name){
+    let copyofCats = [...cats.slice(0,2)];
+    return copyofCats
+}
+function removeFirstCat(name){
+    let copyofcopyofCats = [...cats.slice(1)];
+    return copyofcopyofCats
+}
